48085b9a162157a1c44cf76294eda2d0dadcd42b
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2023-10-26 16:41:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="better-ffmpeg-async",
     version="1.0",
-    packages=["better-ffmpeg-async"],
+    packages=["better_ffmpeg_async"],
     install_requires=[
         "ffmpeg-python",
         "tqdm",
